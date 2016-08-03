01af3978cd29feaabe9eb5000d962e8f0a60ab79
Ricardolv <richardluizv@gmail.com>
2016-08-03 15:49:42 -0300

    first commit

---
+++ src/main/java/com/richard/SpringBootOauth2SecurityApplication.java
@@ -0,0 +1,12 @@
+package com.richard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2SecurityApplication.class, args);
+	}
+}
---
+++ src/main/java/com/richard/security/Authorities.java
@@ -0,0 +1,8 @@
+package com.richard.security;
+
+public enum Authorities {
+
+    ROLE_ANONYMOUS,
+    ROLE_USER,
+    ROLE_ADMIN
+}
